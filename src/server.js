d03c1a8dac5c8bc4908a86144534935ee58e1897
Pranjal BHaskar <97526424+pranjalbhaskar-27@users.noreply.github.com>
2022-06-20 20:54:03 +0530

    get done

--- src/server.js
+++ src/server.js
@@ -7,9 +7,22 @@ let app = express();
 // Code here
 const PORT=8000;
 
+//GET
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname + "../../src/assets/users.html"));
+    console.log('get success')
+    res.sendFile(path.join(__dirname+"../../src/assets/users.html"));
 });
+
+app.get("/users/:id",(req, res) => {
+    const user= user.
+});
+
+//POST
+app.get("/users",(req, res) => {
+    res.sendFile(path.join(__dirname+"../../src/assets/user.json"));
+});
+
+
 
 
 app.listen(PORT,()=>{
