300abb5ef10fcc13dbc251648795f32a17bdbfe0
Pranjal BHaskar <97526617+Debasree-3031999@users.noreply.github.com>
2022-06-20 20:20:31 +0530

    get done

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 // install and import express
-const express = () => {};
+const express=require('express')
+const path=require('path')
+// const express = () => {};
 let app = express();
 
 // Code here
+const PORT=8000;
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname + "../../src/assets/users.html"));
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`listening on port: ${PORT}`)
+})
 // Note: Do not remove this export statement
 module.exports = app;
